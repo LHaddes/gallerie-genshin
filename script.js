64b4b813fcbffdfda6376523f52adda0ac39214a
Kilian <haddesgaming@gmail.com>
2024-04-16 20:07:53 +0200

    Ajout javasript + début lightbox

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal() {
+  document.getElementsByClassName('modal')[0].style.display = "flex";
+
+}
+
+function closeModal() {
+  document.getElementsByClassName('modal')[0].style.display = "none";
+}
