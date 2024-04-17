141a487705ff42ade4b64129e977d2ceeebf9aae
Kilian <haddesgaming@gmail.com>
2024-04-17 15:23:19 +0200

    décalage de la light box résolu + affichage de n'importe quelle image automatiquement dans la lightbox

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-function openModal() {
-  document.getElementsByClassName('modal')[0].style.display = "flex";
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+
+function openModal(image) {
+  // ON BLOQUE LE SCROLLING
+  document.getElementsByClassName('modal')[0].style.display = "flex";
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+
+  //ON RECUPERE L'ID ET L'ALT DE L'IMG CLIQUEE POUR L'AFFICHER AUTOMATIQUEMENT
+  document.getElementById("modalImg").src = image.getElementsByTagName("img")[0].src;
+  document.getElementById("modalImg").alt = image.getElementsByTagName("img")[0].alt;
 }
 
 function closeModal() {
+  //ON REACTIVE LE SCROLLING
   document.getElementsByClassName('modal')[0].style.display = "none";
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
